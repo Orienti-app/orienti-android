52de90f0d8fc97643fa07d07bb226f3360dff301
Michal Záhradník <Nono3551@gmail.com>
2021-09-25 21:29:41 +0200

    Showing run control points

--- app/src/main/java/app/orienti/android/ui/screens/runner/main/ControlPointsAdapter.kt
+++ app/src/main/java/app/orienti/android/ui/screens/runner/main/ControlPointsAdapter.kt
@@ -3,19 +3,25 @@ package app.orienti.android.ui.screens.runner.main
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import app.orienti.android.R
 import app.orienti.android.databinding.UiComponentControlPointRowBinding
 import sk.backbone.parent.ui.screens.ParentRecyclerAdapter
+import sk.backbone.parent.utils.getDifferenceIn24HFormat
+import java.util.*
 
 class ControlPointsAdapter(context: Context): ParentRecyclerAdapter<Boolean, ControlPointsAdapter.ControlPointViewHolder>(context) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ControlPointViewHolder {
         return ControlPointViewHolder(UiComponentControlPointRowBinding.inflate(LayoutInflater.from(context), parent, false))
     }
 
-    class ControlPointViewHolder(val viewBinding: UiComponentControlPointRowBinding): ParentRecyclerViewHolder<Boolean>(viewBinding.root) {
+    class ControlPointViewHolder(private val viewBinding: UiComponentControlPointRowBinding): ParentRecyclerViewHolder<Boolean>(viewBinding.root) {
         override fun bindData(viewData: Boolean) {
             viewBinding.scanned.isChecked = viewData
-            viewBinding.name.text = "Wiiii"
-            viewBinding.time.text = "asdaslkjdlksajdklsajdlkasdasdadsadasdsadada"
+            val timeInPast = Date(Date().time - 49234728934)
+            val now = Date()
+
+            viewBinding.name.text = "Wiii"
+            viewBinding.time.text = viewBinding.root.context.getString(R.string.activity_main_runner_scanned_in_format).format(getDifferenceIn24HFormat(timeInPast, now))
         }
     }
 }
